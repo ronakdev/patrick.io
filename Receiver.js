2c1e91a37817a451475ab8ef3f5e15f8e886108a
Ronak Shah <ronakshahultimate@gmail.com>
2018-09-09 05:22:15 -0400

    works

--- Receiver.js
+++ Receiver.js
@@ -11,16 +11,25 @@ module.exports = class Receiver {
 		grepNoQuant.stdout.on('data', function(data) { 
 
 		  let string = data.toString("utf8")
-		  console.log(`STRING: ${string}`)
-		  onDataCallback(string)
 		  let dataRows = string.split('\n')
 		  dataRows.forEach((row) => {
 		  	let array = row.split(' ').slice(4)
+		  	// a1 a2 a3 a4
+		  	// time
+		  	let b1 = (parseInt(array[0]) + parseInt(array[2])) / 2
+		  	let b2 = (parseInt(array[1]) + parseInt(array[1])) / 2
+		  	if ((b1) && (b2)) {
+		  		let toReturn = ["useless", b1, b2]
+		  		onDataCallback(toReturn)
+		  	} else {
+		  		// pass
+		  	}
+		  	
+
 
 		  	// now we have each row of 
 		  	// 1536415048.86 /muse/eeg ffff 781.37 677.73 669.51 824.14
 		  	// formatted strings
-		  	onDataCallback(array.map((a => parseInt(a)))) // choose 0-3
 		  })
 
 		});
